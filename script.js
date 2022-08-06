8a4934883c96c01b40bc6d574620c0157ebdfc9e
Thigo Dev <thiagomvk08@gmail.com>
2022-08-06 16:08:40 -0300

    commit initial

---
+++ script.js
@@ -0,0 +1,23 @@
+const result = document.getElementById("res");
+
+function calcular() {
+    const nome = document.getElementById("nome").value;
+    const peso = document.getElementById("peso").value;
+    const altura = document.getElementById("altura").value;
+    let imc = (peso / (altura * altura)).toFixed(2);
+    
+    if (imc < 18.5) {
+        result.innerHTML = `Olá ${nome}, seu IMC é ${imc} e você está abaixo do peso ideal`;
+    } else if (imc < 24.9) {
+        result.innerHTML = `Olá ${nome}, seu IMC é ${imc} e você está com  o peso ideal`;
+    } else if (imc < 29.9) {
+        result.innerHTML = `Olá ${nome}, seu IMC é ${imc} e você está acima peso ideal`;
+    } else if (imc < 34.9) {
+        result.innerHTML = `Olá ${nome}, seu IMC é ${imc} e você com obesidade grau I`;
+    } else if (imc < 39.9) {
+        result.innerHTML = `Olá ${nome}, seu IMC é ${imc} e você com obesidade grau II`; 
+    } else {
+        result.innerHTML = `Olá ${nome}, seu IMC é ${imc} e você com obesidade grau III`; 
+    }      
+}
+
