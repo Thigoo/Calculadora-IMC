e8775768babf5dd55bad64744422788790fc93eb
Thigo Dev <thiagomvk08@gmail.com>
2022-08-07 18:09:38 -0300

    função reload no botao

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const result = document.getElementById("res");
 
+
 function calcular() {
     const nome = document.getElementById("nome").value;
     const peso = document.getElementById("peso").value;
     const altura = document.getElementById("altura").value;
     let imc = (peso / (altura * altura)).toFixed(2);
-    
-    if (imc < 18.5) {
+
+    if (nome == "" || peso == "" || altura == "") {
+        result.innerHTML = "Preencha corretamente as informações";
+    } else if (imc < 18.5) {
         result.innerHTML = `Olá ${nome}, seu IMC é ${imc} e você está abaixo do peso ideal`;
     } else if (imc < 24.9) {
         result.innerHTML = `Olá ${nome}, seu IMC é ${imc} e você está com  o peso ideal`;
@@ -15,9 +18,15 @@ function calcular() {
     } else if (imc < 34.9) {
         result.innerHTML = `Olá ${nome}, seu IMC é ${imc} e você com obesidade grau I`;
     } else if (imc < 39.9) {
-        result.innerHTML = `Olá ${nome}, seu IMC é ${imc} e você com obesidade grau II`; 
+        result.innerHTML = `Olá ${nome}, seu IMC é ${imc} e você com obesidade grau II`;
     } else {
-        result.innerHTML = `Olá ${nome}, seu IMC é ${imc} e você com obesidade grau III`; 
-    }      
+        result.innerHTML = `Olá ${nome}, seu IMC é ${imc} e você com obesidade grau III`;
+    }
+
+    var botao = document.getElementById("calcular");
+    botao.innerHTML = "Calcular novamente";
+    botao.addEventListener("click", () => {
+        location.reload();
+    })
 }
 
